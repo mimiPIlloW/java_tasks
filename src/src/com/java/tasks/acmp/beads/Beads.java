23ad266b4e06201c14cff2b8b70b749aaa1d434c
mimiPIllow <erenstonxemin@gmail.com>
2022-05-05 12:01:22 +0300

    acmp (beads)

---
+++ src/src/com/java/tasks/acmp/beads/Beads.java
@@ -0,0 +1,13 @@
+package com.java.tasks.acmp.beads;
+
+import java.util.Scanner;
+
+public class Beads {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input ");
+        int numb = scanner.nextInt();
+
+        System.out.println("Output " + (numb + 1));
+    }
+}
