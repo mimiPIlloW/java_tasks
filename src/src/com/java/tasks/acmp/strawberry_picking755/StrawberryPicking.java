73daa23a6d1256a15be70654525b0c775c801402
mimiPIllow <erenstonxemin@gmail.com>
2022-05-12 11:08:41 +0300

    acmp (strawberry picking)

--- src/src/com/java/tasks/acmp/strawberry_picking755/StrawberryPicking.java
+++ src/src/com/java/tasks/acmp/strawberry_picking755/StrawberryPicking.java
@@ -1,5 +1,6 @@
 package com.java.tasks.acmp.strawberry_picking755;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class StrawberryPicking {
@@ -10,6 +11,11 @@ public class StrawberryPicking {
         int[] array = new int[a];
         for (int b = 0; b < a; b++) {
             array[b] = scanner.nextInt();
+        }
+        if (array[0] + array[1] - array[2] >= 0) {
+            System.out.println(array[0] + array[1] - array[2]);
+        } else {
+            System.out.println("Impossible");
         }
     }
 }
